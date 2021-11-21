3cdb14c226aa318e8a964a6469a07f0acf91a51a
nicolas <capalbo.nicolas@gmail.com>
2021-11-21 23:40:47 +0100

    ledger controller refactor

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'ledgers#index'
-  resources :ledgers, except: [:index, :show] do
+  resources :ledgers do
     resources :dashboard
     resources :accounts
     resources :transactions
