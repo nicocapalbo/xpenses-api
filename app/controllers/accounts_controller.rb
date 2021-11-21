48f2a27eaf23d9b97cf24ab967c709ca197696f4
nicolas <capalbo.nicolas@gmail.com>
2021-11-21 23:26:57 +0100

    accounts controller index refactor

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,15 +1,16 @@
 class AccountsController < ApplicationController
-  # before_action :set_ledger, only: [:index, :create]
-  # def index
-  #   @accounts = Account.where(ledger: @ledger)
-  #   @account = Account.new
-  #   @total_info = {
-  #     total_cleared: total_cleared,
-  #     total_balance: total_balance,
-  #     diff: diff(total_balance, total_cleared),
-  #     diff_percentage: diff_percentage(total_balance, total_cleared)
-  #   }
-  # end
+  before_action :set_ledger, only: [:index, :create]
+
+  def index
+    @accounts = Account.where(ledger: @ledger)
+    @total_info = {
+      total_cleared: total_cleared,
+      total_balance: total_balance,
+      diff: diff(total_balance, total_cleared),
+      diff_percentage: diff_percentage(total_balance, total_cleared)
+    }
+    render json: {accounts: @accounts, totals: @total_info}
+  end
 
   # def create
   #   @account = Account.new(account_params)
@@ -21,29 +22,29 @@ class AccountsController < ApplicationController
   #   end
   # end
 
-  # private
+  private
 
-  # def set_ledger
-  #   @ledger = Ledger.find_by(id: params[:ledger_id])
-  # end
+  def set_ledger
+    @ledger = Ledger.find_by(id: params[:ledger_id])
+  end
 
-  # def account_params
-  #   params.require(:account).permit(:name)
-  # end
+  def account_params
+    params.require(:account).permit(:name)
+  end
 
-  # def diff(balance, cleared)
-  #   balance - cleared
-  # end
+  def diff(balance, cleared)
+    balance - cleared
+  end
 
-  # def diff_percentage(balance, cleared)
-  #   (1 - (balance / cleared)) * 100
-  # end
+  def diff_percentage(balance, cleared)
+    (1 - (balance / cleared)) * 100
+  end
 
-  # def total_cleared
-  #   @ledger.ledger_cleared_deposit - @ledger.ledger_cleared_payment
-  # end
+  def total_cleared
+    @ledger.ledger_cleared_deposit - @ledger.ledger_cleared_payment
+  end
 
-  # def total_balance
-  #   @ledger.ledger_balanced_deposit - @ledger.ledger_balanced_payment
-  # end
+  def total_balance
+    @ledger.ledger_balanced_deposit - @ledger.ledger_balanced_payment
+  end
 end
