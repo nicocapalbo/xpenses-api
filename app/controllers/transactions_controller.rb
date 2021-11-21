9f3b063922f09ce81bd07ebc04b7cc4a3f6e7524
nicolas <capalbo.nicolas@gmail.com>
2021-11-21 23:14:38 +0100

    transaction controller adjust for API calls

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,26 +1,24 @@
 class TransactionsController < ApplicationController
-  # before_action :set_ledger, only: [:index, :create]
+  before_action :set_ledger, only: [:index, :create, :update]
 
-  # def index
-  #   @transactions = Transaction.where(ledger: @ledger).order(date: :desc)
-  #   @transaction = Transaction.new
-  # end
+  def index
+    @transactions = Transaction.where(ledger: @ledger).order(date: :desc)
+    render json: @transactions
+  end
 
-  # def create
-  #   @transaction = Transaction.new(transaction_params)
-  #   @ledger = params[:transaction][:ledger_id].to_i
-  #   if @transaction.save!
-  #     redirect_to ledger_dashboard_index_path(@ledger)
-  #   end
-  # end
+  def create
+    @transaction = Transaction.new(transaction_params)
+    # @ledger = params[:transaction][:ledger_id].to_i
+    return unless @transaction.save
+  end
 
-  # private
+  private
 
-  # def set_ledger
-  #   @ledger = Ledger.find_by(id: params[:ledger_id])
-  # end
+  def set_ledger
+    @ledger = Ledger.find_by(id: params[:ledger_id])
+  end
 
-  # def transaction_params
-  #   params.require(:transaction).permit(:ledger_id, :account_id, :date, :info, :subcategory_id, :cleared, :payment, :deposit)
-  # end
+  def transaction_params
+    params.require(:transaction).permit(:ledger_id, :account_id, :date, :info, :subcategory_id, :cleared, :ttype, :value)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "ledgers#index"
+  root to: 'ledgers#index'
   resources :ledgers, except: [:index, :show] do
     resources :dashboard
     resources :accounts
