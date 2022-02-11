3f4bf2fbb56f5a08907127de18832336e34a8498
nicolas <capalbo.nicolas@gmail.com>
2022-02-11 17:23:05 +0100

    create Ledger with uuid reference; find legers by UUID

--- app/controllers/ledgers_controller.rb
+++ app/controllers/ledgers_controller.rb
@@ -1,12 +1,18 @@
 class LedgersController < ApplicationController
   include Secured
   def index
-    @ledgers = Ledger.all
-    render json: { ledgers: @ledgers, user: current_user }
+    @ledger = Ledger.find_by(uuid: current_user)
+    render json: { ledgers: @ledger, user: current_user }
   end
+
+  # def show
+  #   @ledger = Ledger.find(uuid: current_user)
+  #   render json: { ledger: @ledger, user: current_user }
+  # end
 
   def create
     @ledger = Ledger.new(ledger_params)
+    @ledger.uuid = current_user
     return unless @ledger.save!
   end
 
